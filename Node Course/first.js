5f7e58226a477a27e706098771cda9157a6cfd7d
Vishwas <159616254+vishwas8671@users.noreply.github.com>
2026-05-04 03:37:28 +0530

    Fix  issues in first.js

--- Node Course/first.js
+++ Node Course/first.js
@@ -1,7 +1,6 @@
 console.log("KG Coding is the best...")
 
 const fs = require('fs');
-
 fs.writeFile("Output.txt", "Writing File", (err) => {
     if(err) console.log("Error Occured");
     else console.log("file written successfully");
